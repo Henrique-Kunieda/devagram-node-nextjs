8633ed8ccd54d52623129b54ba9aa582be8fb6f8
Henrique-Kunieda <henriquekunieda.hk@gmail.com>
2024-06-11 21:29:08 -0300

    Conexão ao Banco de Dados

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,17 +1,20 @@
 import type {NextApiRequest,NextApiResponse} from 'next'
-import {conectarMongoDb} from '../../middlewars/conectarMongodb';
+import {conectarMongoDb} from '../../middlewars/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import md5 from 'md5';
+import { UsuarioModel } from '@/models/UsuarioModel';
 
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest, 
     res : NextApiResponse<RespostaPadraoMsg>
 ) =>{
     if(req.method === 'POST'){
         const {login,senha} = req.body;
-
-        if(login === 'admin@admin.com' &&
-            senha === 'Admin@123'){
-               return res.status(200).json({msg : 'Usuário e senha autenticados com sucesso'})
+        
+        const usuarioEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)})
+        if(usuarioEncontrados && usuarioEncontrados.length > 0 ){
+            const usuarioLogado = usuarioEncontrados[0];
+               return res.status(200).json({msg : `Usuário ${usuarioLogado.nome} autenticado com sucesso`})
             }
              return res.status(405).json({erro : ' Usuário ou senha não encontrados'})
     }
